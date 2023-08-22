c273a9c8478d5ecb8295d2ba5386fdd9e182d2cf
ankushKun <ankush4singh@gmail.com>
2023-08-22 11:05:21 +0530

    matrix mult

---
+++ semester-3/java/MatrixMult.java
@@ -0,0 +1,35 @@
+class MatrixMult {
+    public static void main(String[] args) {
+        int m1[][] = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 },
+        };
+        int m2[][] = {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
+        };
+
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1[i].length; j++) {
+                System.out.print(m1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < m2.length; i++) {
+            for (int j = 0; j < m2[i].length; j++) {
+                System.out.print(m1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // add
+        for (int i = 0; i < m1.length; i++) {
+
+        }
+
+    }
+}
